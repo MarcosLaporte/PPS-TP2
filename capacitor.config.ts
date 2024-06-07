c971af2915a7665e2ebb6385c59d6096bc7e9670
LapMarcos <marcoslaporte2015@gmail.com>
2024-06-07 19:44:16 -0300

    Base

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'PPS-SP-La-Comanda',
+  appId: 'com.comanda.app',
+  appName: 'La Comanda',
   webDir: 'www'
 };
 
