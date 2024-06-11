77cba00653a1ee7d2e33e82c251e9997f38862c0
YoberMarlow29 <yober2828@gmail.com>
2024-06-11 12:12:22 -0300

    qr funcionando

--- src/app/pages/alta-cliente/alta-cliente.page.ts
+++ src/app/pages/alta-cliente/alta-cliente.page.ts
@@ -23,64 +23,35 @@ const datePipe = new DatePipe('en-US', '-0300');
     IonIcon, IonFab, IonInput, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule,ReactiveFormsModule]
 })
 export class AltaClientePage implements OnInit {
-  storageName: string = Colecciones.Usuarios;
-  @Input({ required: true }) newPicPrefix!: string;
-
-  picture!: File;
-  tipoCliente: TipoCliente = 'registrado';
+  @Input() newPicPrefix!: string;
 
   frmCliente: FormGroup;
-  nombre = new FormControl('', [Validators.required]);
-  apellido = new FormControl('');
-  DNI = new FormControl(0);
-  email = new FormControl('', [Validators.email]);
-
-  nombreErrorMessage: string = '';
-  apellidoErrorMessage: string = '';
-  DNIErrorMessage: string = '';
-  emailErrorMessage: string = '';
-
+  tipoCliente: TipoCliente = 'registrado';
+  picture!: File;
   isSupported = false;
-  pressedButton: boolean = false;
-  scanActive: boolean = false;
+  scanActive = false;
 
   constructor(
-    private db: DatabaseService,
-    private storage: StorageService,
     private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private storageService: StorageService,
     private scanService: ScannerService
   ) {
     this.frmCliente = this.formBuilder.group({
-      nombre: this.nombre,
-      apellido: this.apellido,
-      DNI: this.DNI,
-      email: this.email,
+      nombre: ['', [Validators.required]],
+      apellido: ['', [Validators.required]],
+      DNI: [0, [Validators.required]],
+      email: ['', [Validators.email, Validators.required]],
+      password: ['', [Validators.required]]
     });
   }
-
-  readonly supportedImageFormats = ['jpg', 'jpeg', 'png'];
 
   ngOnInit() {
     this.scanService.isScannerSupported().then((supported) => {
-      this.isSupported = supported; // Usa ScanService para verificar la compatibilidad del escáner
+      this.isSupported = supported;
     });
   }
-  fillFields() {
-    this.nombre.setValue('Nombre');
-    this.apellido.setValue('Apellido');
-    this.DNI.setValue(12345);
-    this.email.setValue('email@example.com');
-  }
-
-  updateErrorMessage(frmControl: FormControl) {
-    if (frmControl.hasError('required')) {
-      if (frmControl === this.nombre) {
-        this.nombreErrorMessage = 'Este campo es obligatorio';
-      }
-    } else if (frmControl.hasError('email')) {
-      this.emailErrorMessage = 'El formato es incorrecto';
-    }
-  }
+  readonly supportedImageFormats = ['jpg', 'jpeg', 'png'];
 
   async takePic() {
     try {
@@ -124,79 +95,59 @@ export class AltaClientePage implements OnInit {
       }
     }
   }
-
-  private async getFileFromUri(fileUri: string, fileFormat: string): Promise<File> {
-    const response = await fetch(fileUri);
-    const blob = await response.blob();
-    const file = new File([blob], 'photo.jpg', { type: 'image/' + fileFormat });
-    return file;
-  }
-
-  async uploadPicture(image: File) {
-    const datetime: Date = new Date();
-    const dateStr: string = datePipe.transform(datetime, 'yyyyMMdd-HHmmss')!;
-    const nombreFoto: string = `${this.nombre.value}-${this.apellido.value}-${dateStr}`;
-
-    try {
-      const url = await this.storage.subirArchivo(
-        image,
-        `${this.storageName}/${this.newPicPrefix}-${nombreFoto}`
-      );
-      const fotoDePerfil = {
-        id: '',
-        name: nombreFoto,
-        date: datetime,
-        url: url,
-      };
-      console.log(fotoDePerfil);
-      await this.db.subirDoc(this.storageName, fotoDePerfil, true);
-      return url;
-    } catch (error: any) {
-      return null;
-    }
-  }
-
   async subirCliente() {
-    let foto = await this.uploadPicture(this.picture);
+    try {
+      const fotoUrl = await this.uploadPicture(this.picture);
+      if (fotoUrl) {
+        const cliente = new Cliente(
+          '',
+          this.frmCliente.value.nombre,
+          this.frmCliente.value.apellido,
+          this.frmCliente.value.DNI,
+          fotoUrl,
+          this.frmCliente.value.email,
+          this.tipoCliente
+        );
 
-    if (foto) {
-      let cliente = new Cliente(
-        '',
-        this.nombre.value!,
-        this.apellido.value!,
-        this.DNI.value ? Number(this.DNI.value) : 0,
-        foto,
-        this.email.value || '',
-        this.tipoCliente
-      );
+        await this.authService.registrarFireAuth(cliente, this.frmCliente.value.password);
+        MySwal.fire('Cliente registrado con éxito');
+      }
+    } catch (error) {
 
-      await this.db.subirDoc('clientes', cliente, true);
-      console.log('Cliente subido con éxito');
+      console.log("error",error)
+
     }
   }
-
 
   async scan() {
-    this.pressedButton = true; // Indica que el botón de escaneo fue presionado
-    setTimeout(async () => {
-      this.pressedButton = false; // Reinicia el estado del botón después del retraso
-      this.scanActive = true; // Activa el estado de escaneo
-
-      const barcodes = await this.scanService.scanBarcodes(); // Usa ScanService para escanear códigos de barras
-      this.processBarcodes(barcodes); // Procesa los códigos de barras escaneados
-
-      this.scanActive = false; // Desactiva el estado de escaneo
-    }, 2000); // Retraso de 2 segundos antes de iniciar el escaneo
+    this.scanActive = true;
+    const barcodes = await this.scanService.scanBarcodes();
+    this.processBarcodes(barcodes);
+    this.scanActive = false;
   }
 
   processBarcodes(barcodes: Barcode[]) {
     barcodes.forEach(barcode => {
-      const dniData = this.scanService.extractDniData(barcode.rawValue); // Usa ScanService para extraer datos del DNI
+      const dniData = this.scanService.extractDniData(barcode.rawValue);
       if (dniData) {
-        this.DNI.setValue(dniData.dni);
-        this.nombre.setValue(dniData.nombre);
-        this.apellido.setValue(dniData.apellido);
+        this.frmCliente.patchValue({
+          DNI: dniData.dni,
+          nombre: dniData.nombre,
+          apellido: dniData.apellido
+        });
       }
     });
+  }
+
+  private async getFileFromUri(fileUri: string, fileFormat: string): Promise<File> {
+    const response = await fetch(fileUri);
+    const blob = await response.blob();
+    return new File([blob], `photo.${fileFormat}`, { type: `image/${fileFormat}` });
+  }
+
+  private async uploadPicture(image: File): Promise<string> {
+    const dateStr = new Date().toISOString().replace(/[:.]/g, '-');
+    const nombreFoto = `${this.frmCliente.value.nombre}-${this.frmCliente.value.apellido}-${dateStr}`;
+    return this.storageService.subirArchivo(image, `${Colecciones.Usuarios}/${this.newPicPrefix}-${nombreFoto}`);
   }
 }
