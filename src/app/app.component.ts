b4a686a42432ace32b2a76bb27485f89405508da
LapMarcos <marcoslaporte2015@gmail.com>
2024-06-15 15:15:40 -0300

    Redirección al cargar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,21 @@ import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonItem, IonTitle } from '@ionic/angular/standalone';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
   standalone: true,
   imports: [IonTitle, IonItem, IonToolbar, IonHeader, IonApp, IonRouterOutlet, CommonModule, NgxSpinnerModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
-  constructor(protected router: Router) {
+  constructor(protected router: Router, protected auth: AuthService) {
+    const ssUser = sessionStorage.getItem('usuario');
+    this.auth.UsuarioEnSesion = ssUser ? JSON.parse(ssUser) : null;
+
     router.navigateByUrl('splash');
   }
 }
