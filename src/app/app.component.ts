2babcaca6cb016a2acac0131c7dac84c3317ce4a
LapMarcos <marcoslaporte2015@gmail.com>
2024-06-18 14:46:25 -0300

    Menu modal y funciones en FAB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonItem, IonTitle } from '@ionic/angular/standalone';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { AuthService } from './services/auth.service';
-import { skullSharp } from 'ionicons/icons';
+import { scan, logOutOutline, menuOutline, chevronDownCircle } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
+import { MySwal, ToastInfo, ToastSuccess } from './utils/alerts';
+import { AlertController, NavController } from '@ionic/angular';
+import { ScannerService } from './services/scanner.service';
+import { BarcodeFormat } from '@capacitor-mlkit/barcode-scanning';
 
 @Component({
   selector: 'app-root',
@@ -16,20 +20,65 @@ import { addIcons } from 'ionicons';
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
-  public paginas = [
-    { titulo: 'Mesas', url: '/mesas', icono: 'skull' },
-    { titulo: 'Productos', url: '/productos', icono: 'skull' },
-    { titulo: 'Clientes', url: '/clientes', icono: 'skull' },
-    { titulo: 'Encuestas', url: '/encuestas', icono: 'skull' },
+  public grupos = [
+    {
+      nombre: 'General',
+      paginas: [{ titulo: 'Inicio', url: '/home', icono: 'house-chimney' }]
+    },
+    {
+      nombre: 'Altas',
+      paginas: [
+        { titulo: 'Mesa', url: '/alta-mesa', icono: 'table-picnic' },
+        { titulo: 'Empleado', url: '/alta-empleado', icono: 'room-service' },
+        { titulo: 'Cliente', url: '/alta-cliente', icono: 'review' },
+        { titulo: 'Producto', url: '/alta-producto', icono: 'utensils' },
+        { titulo: 'Supervisor', url: '/alta-supervisor', icono: 'boss' },
+      ]
+    }
   ];
 
-  constructor(protected router: Router, protected auth: AuthService) {
+  public funciones = [
+    { titulo: 'Cerrar sesión', icono: 'log-out-outline', accion: async () => await this.cerrarSesion() },
+    { titulo: 'Escanear', icono: 'scan', accion: async () => await this.scanner.escanear() },
+  ]
+
+  constructor(protected router: Router, protected navCtrl: NavController, protected auth: AuthService, private alertCtrl: AlertController, private scanner: ScannerService) {
     const ssUser = sessionStorage.getItem('usuario');
     this.auth.UsuarioEnSesion = ssUser ? JSON.parse(ssUser) : null;
 
-    router.navigateByUrl('splash');
+    navCtrl.navigateRoot('home');
 
-    addIcons({ skullSharp });
+    addIcons({ menuOutline, chevronDownCircle, logOutOutline, scan });
+  }
 
+  itemClick(url: string) {
+    const modal = document.getElementById('pagsModal') as HTMLIonModalElement;
+    modal.dismiss();
+    this.navCtrl.navigateRoot(url)
+  }
+
+  async cerrarSesion() {
+    const alert = await this.alertCtrl.create({
+      header: '¿Desea cerrar sesión?',
+      message: 'No podrá realizar ninguna acción hasta que vuelva a abrir su cuenta.',
+      buttons: [
+        {
+          text: 'No',
+          role: 'cancel',
+          handler: () => ToastInfo.fire('Operación cancelada.')
+          ,
+        },
+        {
+          text: 'Sí',
+          role: 'confirm',
+          handler: async () => {
+            this.auth.signOut();
+            ToastSuccess.fire('Sesión cerrada!');
+          },
+        }
+      ],
+    });
+
+    await alert.present();
   }
 }
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonText } from '@ionic/angular/standalone';
+import { Component } from '@angular/core';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonText, IonCard, IonButton, IonList, IonCardTitle, IonCardHeader, IonCardContent, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { peopleOutline, qrCodeOutline, restaurantOutline } from 'ionicons/icons';
+import { ScannerService } from 'src/app/services/scanner.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonText, IonItem, IonContent, IonHeader, IonTitle, IonToolbar]
+  imports: [IonLabel, IonIcon, IonCardContent, IonCardHeader, IonCardTitle, IonList, IonButton, IonCard, IonText, IonItem, IonContent, IonHeader, IonTitle, IonToolbar]
 })
-export class HomePage implements OnInit {
+export class HomePage {
 
-  constructor() { }
-
-  ngOnInit() {
-    console.log();
-    
+  constructor(public scanner: ScannerService) {
+    addIcons({ qrCodeOutline, peopleOutline, restaurantOutline });
   }
 
 }
