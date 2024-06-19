db482f763108bce4c6df4cb80d2f248e40e56103
LapMarcos <marcoslaporte2015@gmail.com>
2024-06-19 09:29:41 -0300

    Saco menú y FAB. Botón de escáner en toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { CUSTOM_ELEMENTS_SCHEMA, Component, inject } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonItem, IonTitle } from '@ionic/angular/standalone';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { AuthService } from './services/auth.service';
-import { scan, logOutOutline, menuOutline, chevronDownCircle, logInOutline } from 'ionicons/icons';
+import { scan } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
-import { ToastInfo, ToastSuccess } from './utils/alerts';
 import { AlertController, NavController } from '@ionic/angular';
 import { ScannerService } from './services/scanner.service';
 
@@ -19,76 +18,12 @@ import { ScannerService } from './services/scanner.service';
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
-  public grupos = [
-    {
-      nombre: 'General',
-      deshabilitado: false,
-      paginas: [{ titulo: 'Inicio', url: '/home', icono: 'house-chimney' }]
-    },
-    {
-      nombre: 'Altas',
-      deshabilitado: true,
-      paginas: [
-        { titulo: 'Mesa', url: '/alta-mesa', icono: 'table-picnic' },
-        { titulo: 'Empleado', url: '/alta-empleado', icono: 'room-service' },
-        { titulo: 'Cliente', url: '/alta-cliente', icono: 'review' },
-        { titulo: 'Producto', url: '/alta-producto', icono: 'utensils' },
-        { titulo: 'Supervisor', url: '/alta-supervisor', icono: 'boss' },
-      ]
-    }
-  ];
-
-  public funciones: { titulo: string, icono: string, accion: () => Promise<any> }[] = [
-    { titulo: 'Sesión', icono: 'log-in-outline', accion: async () => { } },
-    { titulo: 'Escanear', icono: 'scan', accion: async () => await this.scanner.escanear() },
-  ]
-
-  readonly funcIniciarSesion =
-    { titulo: 'Iniciar sesión', icono: 'log-in-outline', accion: async () => this.navCtrl.navigateRoot('login') };
-  readonly funcCerrarSesion =
-    { titulo: 'Cerrar sesión', icono: 'log-out-outline', accion: async () => await this.cerrarSesion() };
-  constructor(protected router: Router, protected navCtrl: NavController, protected auth: AuthService, private alertCtrl: AlertController, private scanner: ScannerService) {
-    auth.usuarioEnSesionObs.subscribe((usuario) => {
-      this.grupos[1].deshabilitado = !usuario;
-      this.funciones[0] = usuario ? this.funcCerrarSesion : this.funcIniciarSesion;
-    });
-
+  constructor(protected router: Router, protected navCtrl: NavController, protected auth: AuthService, protected scanner: ScannerService) {
     const ssUser = sessionStorage.getItem('usuario');
     this.auth.UsuarioEnSesion = ssUser ? JSON.parse(ssUser) : null;
 
     navCtrl.navigateRoot('home');
 
-    addIcons({ menuOutline, chevronDownCircle, logInOutline, logOutOutline, scan });
-  }
-
-  itemClick(url: string) {
-    const modal = document.getElementById('pagsModal') as HTMLIonModalElement;
-    modal.dismiss();
-    this.navCtrl.navigateRoot(url)
-  }
-
-  async cerrarSesion() {
-    const alert = await this.alertCtrl.create({
-      header: '¿Desea cerrar sesión?',
-      message: 'No podrá realizar ninguna acción hasta que vuelva a abrir su cuenta.',
-      buttons: [
-        {
-          text: 'No',
-          role: 'cancel',
-          handler: () => ToastInfo.fire('Operación cancelada.')
-          ,
-        },
-        {
-          text: 'Sí',
-          role: 'confirm',
-          handler: async () => {
-            this.auth.signOut();
-            ToastSuccess.fire('Sesión cerrada!');
-          },
-        }
-      ],
-    });
-
-    await alert.present();
+    addIcons({ scan });
   }
 }
