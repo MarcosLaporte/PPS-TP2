9c12001354c7fa61b827b08f3a214b57f6d3f3cc
LapMarcos <marcoslaporte2015@gmail.com>
2024-06-20 02:20:17 -0300

    Correción general y merge a Test
    +	Paquete de Flaticon en package.json
    +	Funciones globales en main.ts
    +	Botón de escanear en App Component
    +	Archivos enum e interfaces
    +	Estilos e imagen de fondo en el login
    +	Pantalla de bienvenida en Home Component
    ~	global.scss: Colores en variables --background
    ~	ion-outer-outlet dentro de ion-content
    ~	Elimino todos los ion-content y ion-header (fuera de App Component)
    ~	Aplico las funciones globales (main) en componentes de Alta
    ~	AuthService: renombramiento de funciones
    ~	ScannerService: reformulamiento de funciones
    ~	Alta Cliente: nombre de foto del cliente
    ~	Alta Supervisor y Mesa: Comento la función que sube a la BD la foto en vez del objeto

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -3,17 +3,26 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonItem, IonText, IonCard,
 import { addIcons } from 'ionicons';
 import { peopleOutline, qrCodeOutline, restaurantOutline } from 'ionicons/icons';
 import { ScannerService } from 'src/app/services/scanner.service';
+import { NavController } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonLabel, IonIcon, IonCardContent, IonCardHeader, IonCardTitle, IonList, IonButton, IonCard, IonText, IonItem, IonContent, IonHeader, IonTitle, IonToolbar]
+  imports: [IonLabel, IonIcon, IonCardContent, IonCardHeader, IonCardTitle, IonList, IonButton, IonCard, IonText, IonItem, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule]
 })
 export class HomePage {
+  paginas = [
+    'alta-cliente',
+    'alta-producto',
+    'alta-supervisor',
+    'alta-mesa',
+    'alta-empleado'
+  ];
 
-  constructor(public scanner: ScannerService) {
+  constructor(public scanner: ScannerService, protected nav: NavController) {
     addIcons({ qrCodeOutline, peopleOutline, restaurantOutline });
   }
 
