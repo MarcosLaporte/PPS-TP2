ff6fc9f91808fbce75e57237583d5ed807766cee
LapMarcos <marcoslaporte2015@gmail.com>
2024-06-26 15:34:13 -0300

    Más campos para componente RangoEstrellas

--- src/app/components/range-estrellas/range-estrellas.component.ts
+++ src/app/components/range-estrellas/range-estrellas.component.ts
@@ -1,19 +1,38 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { IonLabel } from "@ionic/angular/standalone";
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-range-estrellas',
   templateUrl: './range-estrellas.component.html',
   styleUrls: ['./range-estrellas.component.scss'],
   standalone: true,
-  imports: [IonLabel]
+  imports: [CommonModule, FormsModule]
 })
-export class RangeEstrellasComponent {
-  @Input() titulo: string = 'Puntuación 0-5';
+export class RangeEstrellasComponent implements AfterViewInit {
+  @Input() titulo?: string;
+  /**
+   * Si el componente se va a usar múltiples veces en un mismo display, el id de los input deben ser diferentes.
+   */
+  @Input({ required: true }) idEstrellas!: string;
+  @Input() min: number = 0;
+  @Input() max: number = 5;
+  @Input() step: number = 0.5;
+  @Input() valor: number = 5;
+  @Input() readonly: boolean = false;
   @Output() cambioValor = new EventEmitter<number>();
 
+  private estrellasEl!: HTMLElement;
+  ngAfterViewInit() {
+    this.estrellasEl = document.getElementById(this.idEstrellas)!;
 
-  emit($ev: any) {
-    this.cambioValor.emit($ev.target.value);
+    this.estrellasEl.style.setProperty('--value', `${this.valor}`);
+    this.estrellasEl.style.setProperty('--stars', `${this.max}`);
+  }
+
+  cambiarValor($ev: any) {
+    const nuevoValor = $ev.target.value;
+    this.estrellasEl.style.setProperty('--value', nuevoValor);
+    this.cambioValor.emit(nuevoValor);
   }
 }
