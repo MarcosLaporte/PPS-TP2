7981693e7e1c7b69156f1f37e34f19122bb2e91c
LapMarcos <marcoslaporte2015@gmail.com>
2024-07-01 16:47:00 -0300

    Arreglo alta pedido

--- src/app/utils/classes/pedido.ts
+++ src/app/utils/classes/pedido.ts
@@ -1,31 +1,28 @@
 import { Producto } from "./producto";
 
-
 export class Pedido {
-    id: string;
-    pedidoProd: PedidoArmado[];
-    precio: number;
-    tiempoEstimado: number;
-    estado: EstadoPedido;
+  id: string;
+  pedidoProd: PedidoArmado[];
+  precio: number;
+  tiempoEstimado: number;
+  estado: EstadoPedido;
 
-    constructor( id : string, pedidoProd : PedidoArmado[], precio : number, tiempoEstimado : number){
-        this.id = id;
-        this.pedidoProd = pedidoProd;
-        this.precio = precio;
-        this.tiempoEstimado = tiempoEstimado;
-        this.estado = 'pendiente';
-    }
+  constructor(pedidoProd: PedidoArmado[], precio: number, tiempoEstimado: number) {
+    this.id = '';
+    this.pedidoProd = pedidoProd;
+    this.precio = precio;
+    this.tiempoEstimado = tiempoEstimado;
+    this.estado = 'pendiente';
+  }
 }
 
-export type EstadoPedido = 'pendiente' | 'en proceso'
-
-export interface PedidoProd{
-    producto: Producto,
-    cantidad: number,
+export type EstadoPedido = 'pendiente' | 'en proceso' | 'entregado';
+export interface PedidoProd {
+  producto: Producto,
+  cantidad: number,
 };
-
-export interface PedidoArmado{
-    nombre: string,
-    cantidad: number,
-    tiempoEstimado: number,
+export interface PedidoArmado {
+  nombre: string,
+  cantidad: number,
+  tiempoEstimado: number,
 };
